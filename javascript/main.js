89d169d7348c537a2b11595f39a1cde65d67e4a8
vupham3001 <vu.pt@haposoft.com>
2021-08-20 15:14:38 +0700

    commit feedback

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,25 @@
-
+$('.carousel slide').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
